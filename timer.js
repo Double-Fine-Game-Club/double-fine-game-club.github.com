c3cd04aa8f0863e6b006898ff5d445359d52a876
Janne Enberg <janne.enberg@lietu.net>
2012-10-06 22:54:52 +0300

    Stability improvement, opera fix
    Made the code output stuff to console log if it's available
    Made the JS code repeatedly try initializing chat and timer
    functionalities until it works, it "cleanly fails" when it realizes a
    required element is missing and retries after 100msec

--- timer.js
+++ timer.js
@@ -63,231 +63,319 @@ var eventDOW = 6; // Day of week .. 0-6, where 0 = Sun, 6 = Sat (Saturday)
 var eventTime = 19 * 60 * 60 * 1000; // How many msec from midnight in UTC is the event (19:00)
 var eventLength = 1.5 * 60 * 60 * 1000; // Approximate event length in msec (1.5hrs)
 
-// URL to the mibbit chat
-var chatUrl = 'http://widget.mibbit.com/?server=irc.foonetic.net&amp;channel=%23DFAdventure&amp;nick=GameClubFan_??????&amp;noServerMotd=true&amp;settings=e45b6397f1cdd3f1e3db04091b842551';
-		
-// Get the chat IFRAME
-var chatVisible = false;
-var getChat = (function() {
-
-	// Internal cache
-	var chat = null;
-	
-	// Function to actually return/create it
-	return function(create) {
-		// If no chat element yet, and we're allowed to create it
-		if (chat === null && create) {
-		
-			// Create an IFRAME, set it's source to the chat URL
-			chat = document.createElement('iframe');
-			chat.src = chatUrl;
+var log = function() {
+	if (console && console.log && console.log.apply) {
+		if (arguments.length === 1) {
+			console.log(arguments[0]);
+		} else {
+			console.log(Array.prototype.slice.call(arguments));
 		}
-
-		// Return the element
-		return chat;
 	}
-})();
+}
 
-// Hide the chat element
-var hideChat = function() {
-	// Get the chat, if it exists
-	var chat = getChat(false);
-	// If it did
-	if (chatVisible) {
-		// Remove it from it's parent node
-		chat.parentNode.removeChild(chat);
-		chatVisible = false;
+// Initialize the chat functionality
+var initChat = function() {
+
+	// List of elements we need to exist before we can init chat
+	var requiredElements = [
+		'main',
+		'openChat',
+		'hideChat',
+		'showChat',
+		'live_embed_player_flash'
+	];
+		
+	// Check that all required elements exist, abort if not
+	for (var i=0, count = requiredElements.length; i<count; ++i) {
+		if (!document.getElementById(requiredElements[i])) {
+			return false;
+		}
 	}
 
-};
-
-// Show the chat element
-var showChat = function() {
-	// If the chat is not visible
-	if (!chatVisible) {
-	
-		// Get/create the element
-		var chat = getChat(true);
-
-		// Insert the element to DOM
-		document.getElementById('main').insertBefore(
-			chat, 
-			document.getElementById('live_embed_player_flash').nextSibling
-		);
-		
-		chatVisible = true;
-	}
-	
-};
-
-// "Open chat in a separate window" -link
-document.getElementById('openChat').onclick = function(event) {
-	// Make sure the chat is hidden in this window
-	hideChat();
-	// Allow the normal click to work
-};
-
-// "Hide the chat pane" -link
-document.getElementById('hideChat').onclick = function(event) {
-	hideChat();
-
-	// Stop the default click action from happening
-	event.preventDefault();
-	return false;
-};
-
-// "Restore it" -link
-document.getElementById('showChat').onclick = function(event) {
-	// Just show the chat
-	showChat();
-	
-	// Stop the default click action from happening
-	event.preventDefault();
-	return false;
-};
-
-// Timer update logic
-var updateTimer = (function() {
-
-	// By default we don't know the server time diff
-	var serverTimeDiff = 0;
+	// URL to the mibbit chat
+	var chatUrl = 'http://widget.mibbit.com/?server=irc.foonetic.net&amp;channel=%23DFAdventure&amp;nick=GameClubFan_??????&amp;noServerMotd=true&amp;settings=e45b6397f1cdd3f1e3db04091b842551';
 			
-	// Cache the countdown element
-	var countdownElement = document.getElementById('countdownText');
+	// Get the chat IFRAME
+	var chatVisible = false;
+	var getChat = (function() {
 
-	// Make current time accesible for all the functions in this scope
-	var currentTime = null;
-	
-	var checkServerTime = function() {
-		// If we have server time, calculate the difference to it
-		if (serverTimeDiff === 0 && typeof serverTime !== 'undefined') {
-			serverTimeDiff = (typeof serverTime !== "undefined"? (new Date().getTime()) - Math.floor(serverTime * 1000) : 0);
-			// console.log('Found server time, diff now: ' + serverTimeDiff + ' msec');
-		}
-	};
-
-	// Get the time of the next event
-	var getNextEventTime = function() {
-	
-		// Get a new date for the current time
-		var date = new Date();
+		// Internal cache
+		var chat = null;
 		
-		// Calculate difference in weekdays
-		var dowDiff = date.getUTCDay() - eventDOW;
-		
-		// If not on the event DOW, update day
-		if (dowDiff !== 0) {
-		
-			// Update the day of week				
-			date = new Date(date.getTime() + ((0-dowDiff) * 24 * 60 * 60 * 1000))
+		// Function to actually return/create it
+		return function(create) {
+			// If no chat element yet, and we're allowed to create it
+			if (chat === null && create) {
 			
-		}
-		
-		// Reset to midnight
-		date.setUTCHours(0);
-		date.setUTCMinutes(0);
-		date.setUTCSeconds(0);
-		date.setUTCMilliseconds(0);
-		
-		// And fix the date to the time of the event
-		date = new Date(date.getTime() + eventTime);
-		
-		// But if we've passed the event, add a week
-		if (currentTime > date.getTime() + eventLength) {
-			date = new Date(date.getTime() + (7 * 24 * 60 * 60 * 1000))
-		}
-		
-		// console.log('Next event  : ' + date);
-						
-		return date.getTime();
-	};
-	
-	// Function to format milliseconds to [D days, ]HH:MM:SS text
-	var formatMsec = (function() {
-	
-		// Function to zero-pad a number
-		var zeroPad = function(number) {
-			return (number>=10 ? number : '0'+number);
-		};
-		
-		// Function with the core format logic
-		return function(msec) {
-		
-			// Units we want to calculate
-			var unitList = {
-				days: 24 * 60 * 60 * 1000,
-				hours: 60 * 60 * 1000,
-				minutes: 60 * 1000,
-				seconds: 1000
-			};
-			
-			// Where to store the result
-			var result = {};
-			
-			// Loop through the units
-			for (var unit in unitList) {
-				// Get the value off the list
-				var value = unitList[unit];
-
-				// By default, it doesn't fit
-				var count = 0;
-				
-				// If it does however fit
-				if (msec > value) {
-				
-					// Update how many of these units is in it
-					count = Math.floor(msec / value);
-					
-					// And take off from the remainder
-					msec -= count * value;
-				}
-				
-				// Save result for this unit
-				result[unit] = count;
+				// Create an IFRAME, set it's source to the chat URL
+				chat = document.createElement('iframe');
+				chat.src = chatUrl;
 			}
-			
-			
-			// If there are days remaining, start with the number of days
-			var text = (result.days > 0 ? result.days + ' days, ' : '');
-			
-			// Append the HH:MM:SS
-			text += zeroPad(result.hours) + ':' + zeroPad(result.minutes) + ':' + zeroPad(result.seconds);
-			
-			// Give back the formatted text
-			return text;
-		};
+
+			// Return the element
+			return chat;
+		}
 	})();
 
-	// What to do on each tick of the clock
-	var tick = function() {
-	
-		// Check if we have server time and calculate diff if we do
-		checkServerTime();
-	
-		// Get the current time (unix timestamp in msec)
-		currentTime = new Date().getTime() - serverTimeDiff;
-		// console.log('Current time: ' + new Date(currentTime));
-	
-		// Get the time of the next event
-		var nextEventTime = getNextEventTime();
-		
-		// Get time difference
-		var timeRemaining = nextEventTime - currentTime;
+	// Hide the chat element
+	var hideChat = function() {
+		// Get the chat, if it exists
+		var chat = getChat(false);
+		// If it did
+		if (chatVisible) {
+			// Remove it from it's parent node
+			chat.parentNode.removeChild(chat);
+			chatVisible = false;
+		}
 
-		// Say "Right now!" if it's going on right now, otherwise format the time remaining
-		var text = (timeRemaining <= 0 ? 'Right now!' : formatMsec(timeRemaining));
+	};
+
+	// Show the chat element
+	var showChat = function() {
+		// If the chat is not visible
+		if (!chatVisible) {
 		
-		// Update countdown element
-		countdownElement.innerHTML = text;
+			// Get/create the element
+			var chat = getChat(true);
+
+			// Insert the element to DOM
+			document.getElementById('main').insertBefore(
+				chat, 
+				document.getElementById('live_embed_player_flash').nextSibling
+			);
+			
+			chatVisible = true;
+		}
+		
+	};
+
+	// "Open chat in a separate window" -link
+	document.getElementById('openChat').onclick = function(event) {
+		// Make sure the chat is hidden in this window
+		hideChat();
+		// Allow the normal click to work
+	};
+
+	// "Hide the chat pane" -link
+	document.getElementById('hideChat').onclick = function(event) {
+		hideChat();
+
+		// Stop the default click action from happening
+		event.preventDefault();
+		return false;
+	};
+
+	// "Restore it" -link
+	document.getElementById('showChat').onclick = function(event) {
+		// Just show the chat
+		showChat();
+		
+		// Stop the default click action from happening
+		event.preventDefault();
+		return false;
 	};
 	
-	// Return the tick function as our "external interface"
-	return tick;
-})();
+	// Show the chat
+	showChat();
+	
+	return true;
+};
 
-// Set the timer
-setInterval(updateTimer, 1000);		
+var initTimer = function() {
+	// Timer update logic
+	var updateTimer = (function() {
 
-// And show the chat
-showChat();
+		// By default we don't know the server time diff
+		var serverTimeDiff = 0;
+				
+		// Cache the countdown element
+		var countdownElement = document.getElementById('countdownText');
+		
+		// Check that it actually exists, abort if not
+		if (!countdownElement) {
+			return false;
+		}
+
+		// Make current time accesible for all the functions in this scope
+		var currentTime = null;
+		
+		var checkServerTime = function() {
+			// If we have server time, calculate the difference to it
+			if (serverTimeDiff === 0 && typeof serverTime !== 'undefined') {
+				serverTimeDiff = (typeof serverTime !== "undefined"? (new Date().getTime()) - Math.floor(serverTime * 1000) : 0);
+				log('Found server time, diff now: ' + serverTimeDiff + ' msec');
+			}
+		};
+
+		// Get the time of the next event
+		var getNextEventTime = function() {
+		
+			// Get a new date for the current time
+			var date = new Date();
+			
+			// Calculate difference in weekdays
+			var dowDiff = date.getUTCDay() - eventDOW;
+			
+			// If not on the event DOW, update day
+			if (dowDiff !== 0) {
+			
+				// Update the day of week				
+				date = new Date(date.getTime() + ((0-dowDiff) * 24 * 60 * 60 * 1000))
+				
+			}
+			
+			// Reset to midnight
+			date.setUTCHours(0);
+			date.setUTCMinutes(0);
+			date.setUTCSeconds(0);
+			date.setUTCMilliseconds(0);
+			
+			// And fix the date to the time of the event
+			date = new Date(date.getTime() + eventTime);
+			
+			// But if we've passed the event, add a week
+			if (currentTime > date.getTime() + eventLength) {
+				date = new Date(date.getTime() + (7 * 24 * 60 * 60 * 1000))
+			}
+			
+			log('Next event  : ' + date);
+							
+			return date.getTime();
+		};
+		
+		// Function to format milliseconds to [D days, ]HH:MM:SS text
+		var formatMsec = (function() {
+		
+			// Function to zero-pad a number
+			var zeroPad = function(number) {
+				return (number>=10 ? number : '0'+number);
+			};
+			
+			// Function with the core format logic
+			return function(msec) {
+			
+				// Units we want to calculate
+				var unitList = {
+					days: 24 * 60 * 60 * 1000,
+					hours: 60 * 60 * 1000,
+					minutes: 60 * 1000,
+					seconds: 1000
+				};
+				
+				// Where to store the result
+				var result = {};
+				
+				// Loop through the units
+				for (var unit in unitList) {
+					// Get the value off the list
+					var value = unitList[unit];
+
+					// By default, it doesn't fit
+					var count = 0;
+					
+					// If it does however fit
+					if (msec > value) {
+					
+						// Update how many of these units is in it
+						count = Math.floor(msec / value);
+						
+						// And take off from the remainder
+						msec -= count * value;
+					}
+					
+					// Save result for this unit
+					result[unit] = count;
+				}
+				
+				
+				// If there are days remaining, start with the number of days
+				var text = (result.days > 0 ? result.days + ' days, ' : '');
+				
+				// Append the HH:MM:SS
+				text += zeroPad(result.hours) + ':' + zeroPad(result.minutes) + ':' + zeroPad(result.seconds);
+				
+				// Give back the formatted text
+				return text;
+			};
+		})();
+
+		// What to do on each tick of the clock
+		var tick = function() {
+		
+			// Check if we have server time and calculate diff if we do
+			checkServerTime();
+		
+			// Get the current time (unix timestamp in msec)
+			currentTime = new Date().getTime() - serverTimeDiff;
+			log('Current time: ' + new Date(currentTime));
+		
+			// Get the time of the next event
+			var nextEventTime = getNextEventTime();
+			
+			// Get time difference
+			var timeRemaining = nextEventTime - currentTime;
+
+			// Say "Right now!" if it's going on right now, otherwise format the time remaining
+			var text = (timeRemaining <= 0 ? 'Right now!' : formatMsec(timeRemaining));
+			
+			// Update countdown element
+			countdownElement.innerHTML = text;
+		};
+		
+		// Return the tick function as our "external interface"
+		return tick;
+	})();
+
+	// Set the timer
+	setInterval(updateTimer, 1000);
+	
+	return true;
+};
+
+
+
+// Try initializing the chat and timer code until it works
+var initInterval = null;
+var initCount = 0;
+initInterval = setInterval((function() {
+	var chatOk = false;
+	var timerOk = false;
+
+	return function() {
+		try {
+			initCount++;
+		
+			// Try to initialize chat until it's ok
+			if (!chatOk) {
+				if (initChat()) {
+					chatOk = true;
+					log('Chat initialized');
+				} else {
+					log('Chat failed to initialize');
+				}
+			}
+			
+			// Try to initialize timer until it's ok
+			if (!timerOk) {
+				if (initTimer()) {
+					timerOk = true;
+					log('Timer initialized');
+				} else {
+					log('Timer failed to initialize');
+				}
+			}
+			
+			// Stop retrying after both are ok
+			if (chatOk && timerOk) {
+				clearInterval(initInterval);
+				console.log('Everything initialized fine after ' + initCount + ' tries, stopping retries.');
+			}
+			
+		} catch (e) {
+			; // Discard errors
+		}
+	};
+})(), 100);
+
+
 })();
